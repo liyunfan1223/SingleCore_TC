a765bb1f68c850ecaf621ca7df510d4444e18b95
Yunfan Li <lyf_liyunfan@sina.com>
2023-03-24 23:59:06 +0800

    fix: try3

--- src/server/scripts/Northrend/Naxxramas/boss_thaddius.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_thaddius.cpp
@@ -324,8 +324,8 @@ public:
                 instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POSITIVE_CHARGE_APPLY);
                 instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_NEGATIVE_CHARGE_APPLY);
 
-                me->DespawnOrUnsummon();
-                me->SetRespawnTime(initial ? 5 : 30);
+                me->DespawnOrUnsummon(0, 30 * IN_MILLISECONDS);
+                // me->SetRespawnTime(initial ? 5 : 30);
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PC | UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_STUNNED);
                 me->setActive(false);
                 // events.SetPhase(PHASE_RESETTING);
