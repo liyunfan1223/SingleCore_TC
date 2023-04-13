6b7d2895d78d7df13bb418c74e84306cc574c5fe
Yunfan Li <lyf_liyunfan@sina.com>
2023-04-13 18:07:32 +0800

    scripts: update squash script

--- scripts/squash_items_guid.py
+++ scripts/squash_items_guid.py
@@ -5,7 +5,7 @@ import MySQLdb # import t/he MySQLdb module
 
 # Create the connection object
 connection = MySQLdb.connect(
-    host="124.223.191.243",
+    host="127.0.0.1",
     user="root",
     passwd="root",
     db="characters",
@@ -20,36 +20,29 @@ cursor.execute("select * from item_instance")
 count = cursor.fetchone()
 idx = 0
 mp = {}
+print("Collecting item guids...")
 while count:
     guid = count[0]
-    # if guid < 1000:
-    #     count = cursor.fetchone()
-    #     continue
     if guid not in mp.keys():
         mp[guid] = idx
-    idx += 1
-    if (idx % 10000 == 0):
-        print(guid, idx)
     count = cursor.fetchone()
+    idx += 1
 
 cursor.close()
-print("!", len(mp), idx)
+print("Collected item guids number:", idx)
 cursor = connection.cursor()
 
-ok = 0
 for key in mp.keys():
     value = mp[key]
-    print("!", key, value)
+    if (value % 1000 == 0):
+        print(f"Turning old guid: {key} -> {value}")
     cursor.execute(f"update item_soulbound_trade_data set itemGuid={value} where itemGuid={key};")
     cursor.execute(f"update character_inventory set item={value} where item={key};")
     cursor.execute(f"update character_inventory set bag={value} where bag={key};")
+    # res = cursor.rowcount
+    # print(res)
     cursor.execute(f"update item_instance set guid={value} where guid={key};")
-    # ok += 1
-    # if (ok % 2000 == 0):
-    #     print("?!?!", ok)
-    #     cursor.close()
-    #     break
-    # cursor.close()
 
+print("Item guid squash succeed.")
 connection.commit()
 connection.close()
